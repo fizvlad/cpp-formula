e3d6dff594abdf03f4242c8b255be52b7436d174
Fizvlad <fizvlad@mail.ru>
2018-05-05 20:57:53 +0300

    edited test. The way it must be used in programm

--- tests/test_1/main.cpp
+++ tests/test_1/main.cpp
@@ -3,10 +3,18 @@
 #include "cpp-formula.h"
 
 using namespace std;
+using namespace fizvlad;
 
 int main()
 {
-    string str = "obj1 + obj2 * obj3 - !(obj1 && obj2) + 1";
+    string str = "obj1 + obj2 * obj3 - -(obj1 + obj2 * (obj4 - (obj5))) + 1";
     cout << "Formula: " << str << endl;
+
+    Formula::Action a_brackets(1, R"RAW_LITERAL(\(([^\(\)]+)\))RAW_LITERAL");
+    Formula::Action a_plus(1, R"RAW_LITERAL((.+)\+(.+))RAW_LITERAL");
+
+    Formula f(str, {a_brackets, a_plus});
+    Formula::Sequence s = f.getSequence();
+
     return 0;
 }
