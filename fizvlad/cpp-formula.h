7ee6e6d49443df242a43003ad17376f02d65359c
Fizvlad <fizvlad@mail.ru>
2018-05-06 13:52:55 +0300

    added main function of formula parsing. Still need to turn array of aliases into sequence

--- fizvlad/cpp-formula.h
+++ fizvlad/cpp-formula.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 #include <regex>
+#include <iostream>
+#include <exception>
 
 namespace fizvlad {
 
@@ -21,12 +23,25 @@ namespace fizvlad {
             size_t operandsAmount;
 
             /// Regex matching this action if all the actions with heigher priority are already initialized. Amount of matching groups must be equal to operandsAmount
-            /// Notice: "ALIAS${id}" will be placed into the initial string please make sure your regexp won't recognise it as operator
-            std::basic_regex<char> regexp;
+            /// Notice: "${aliasPrefix}${id}" will be placed into the initial string please make sure your regexp won't recognise it as operator
+            std::regex regexp;
 
 
-            Action(size_t o = 0, std::string r = "") : operandsAmount(o), regexp(r, std::regex_constants::ECMAScript) {}
-            Action(size_t o, std::basic_regex<char> r) : operandsAmount(o), regexp(r) {}
+            Action(size_t o = 0, std::string r = "") : operandsAmount(o), regexp(r, std::regex_constants::ECMAScript) {
+                regexpCheck_();
+            }
+            Action(size_t o, std::regex r) : operandsAmount(o), regexp(r) {
+                regexpCheck_();
+            }
+        private:
+            bool regexpIsCorrect_() {
+                return regexp.mark_count() == operandsAmount;
+            }
+            void regexpCheck_() {
+                if (!regexpIsCorrect_()) {
+                    throw std::invalid_argument("Number of marked sub-expressions (" + std::to_string(regexp.mark_count()) + ") within the regular expression doesn't match with stated amount of operands (" + std::to_string(operandsAmount) + ")");
+                }
+            }
         };
 
 
@@ -61,18 +76,59 @@ namespace fizvlad {
         typedef std::vector<Action> Actions;
 
 
-        Formula(std::string str, Actions actions) : str_(str), actions_(actions) {}
+        Formula(std::string str, Actions actions, std::string aliasPrefix = "ALIAS_") : str_(str), actions_(actions), aliasPrefix_(aliasPrefix) {}
         Formula() = delete;
+
 
         Sequence getSequence() {
             Sequence result;
-            // TODO Applying regex with highest priority and if got enough matching operands then recursively creating formula for each one.
+            std::vector<std::string> aliases; // Array to store aliases
+            setUpAliases_(str_, &aliases);
+            for (size_t i = 0; i < aliases.size(); i++) {
+                std::cout << aliasPrefix_ + std::to_string(i) << " = " << aliases[i] << std::endl;
+            }
             return result;
         }
+
 
     private:
         std::string str_;
         Actions actions_;
+        std::string aliasPrefix_;
+
+
+        void setUpAliases_(std::string str, std::vector<std::string> *aliases) {
+            for (size_t i = 0; i < actions_.size(); i++) {
+                // For each action:
+                Action action = actions_[i];
+                while (true) {
+                    // While we can still apply current action
+                    std::smatch match;
+                    std::regex_search(str, match, action.regexp);
+                    size_t matchesAmount = match.size();
+                    if (matchesAmount == 0) {
+                        // Stop if can not apply this action
+                        break;
+                    }
+
+                    for (size_t j = 1; j < match.size(); j++) {
+                        // Recursively processing each operand
+                        std::string sub = match[j].str();
+                        setUpAliases_(sub, aliases);
+                    }
+
+                    std::string target = match[0]; // Saving target string
+                    size_t aliasIndex = std::find(aliases->cbegin(), aliases->cend(), target) - aliases->cbegin();
+                    // If this target already been met somewhere index will be correctly set
+                    // Index = aliases.size() otherwise
+                    str.replace(str.find(target), target.length(), aliasPrefix_ + std::to_string(aliasIndex));
+                    if (aliasIndex == aliases->size()) {
+                        // Saving new alias
+                        aliases->push_back(target);
+                    }
+                }
+            }
+        }
     };
 
 }
--- tests/test_1/main.cpp
+++ tests/test_1/main.cpp
@@ -7,14 +7,19 @@ using namespace fizvlad;
 
 int main()
 {
-    string str = "obj1 + obj2 * obj3 - -(obj1 + obj2 * (obj4 - (obj5))) + 1";
+    string str = "obj1 + obj2 * obj3 + obj2 * obj3 - 2 * (obj1 + obj2 * (obj4 - (obj5))) + 1";
     cout << "Formula: " << str << endl;
 
     Formula::Action a_brackets(1, R"RAW_LITERAL(\(([^\(\)]+)\))RAW_LITERAL");
-    Formula::Action a_plus(1, R"RAW_LITERAL((.+)\+(.+))RAW_LITERAL");
+    Formula::Action a_plus(2, R"RAW_LITERAL(([^\s]+) \+ ([^\s]+))RAW_LITERAL");
+    Formula::Action a_minus(2, R"RAW_LITERAL(([^\s]+) \- ([^\s]+))RAW_LITERAL");
+    Formula::Action a_multiple(2, R"RAW_LITERAL(([^\s]+) \* ([^\s]+))RAW_LITERAL");
+    Formula::Action a_divide(2, R"RAW_LITERAL(([^\s]+) \/ ([^\s]+))RAW_LITERAL");
 
-    Formula f(str, {a_brackets, a_plus});
-    Formula::Sequence s = f.getSequence();
+    Formula::Actions actions = {a_brackets, a_multiple, a_divide, a_plus, a_minus};
+
+    Formula f(str, actions);
+    Formula::Sequence sequence = f.getSequence();
 
     return 0;
 }
